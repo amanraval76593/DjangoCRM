d398024af7b0659bcee6cb9ec7206a695c47a6ca
amanraval76593 <aman76593@gmail.com>
2025-02-14 14:18:28 +0530

    connected database to store records

--- website/views.py
+++ website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    record = Record.objects.all()
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -17,7 +19,7 @@ def home(request):
             messages.error(request, "Invalid credentials")
             return redirect("home")
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"records": record})
 
 
 def logout_user(request):
